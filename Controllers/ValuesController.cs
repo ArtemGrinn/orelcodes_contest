951f4866f23746a3e9645a2b9442445aedaabc7f
ArtemGrinn <ahaletskiy@gmail.com>
2018-11-05 02:22:06 +0300

    refact

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -23,16 +23,24 @@ namespace WebApi.Controllers
         {
             using (var md5Hash = MD5.Create())
             {
-                data.first_name += $" {GetMd5Hash(md5Hash, data.first_name)}";
-                data.last_name += $" {GetMd5Hash(md5Hash, data.last_name)}";
+                data.first_name += GetMd5Hash(md5Hash, data.first_name);
+                data.last_name += GetMd5Hash(md5Hash, data.last_name);
             }
             data.say = "C# is best!";
             data.current_time = DateTime.Now.ToString(DateFormat);
             return Json(data);
         }
 
-        private static string GetMd5Hash(MD5 md5Hash, string input) 
-            =>  BitConverter.ToString(md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input)));
+        private static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));	
+            var sBuilder = new StringBuilder();	
+            sBuilder.Append(" ");	
+            for (byte i = 0; i < data.Length; i++)		
+                sBuilder.Append(data[i].ToString("x2"));	
+            
+            return sBuilder.ToString();
+        }
     }
 
     public class JsonModel
